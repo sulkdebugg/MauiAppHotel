1ef476b9371165fa8cd25a9b74c6af280ae79185
Groth <arthuraquilesb@gmail.com>
2026-05-25 04:44:36 -0300

    Atualização tudo

---
+++ MauiAppHotel/Models/Quarto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppHotel.Models
+{
+    public class Quarto
+    {
+        public string Descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+
+        public double ValorDiariaCrianca { get; set; }
+    }
+}
